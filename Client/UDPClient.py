e33c6e86af2acd639ec039993449ed48876443bb
Osnott <aidenonstott@gmail.com>
2019-01-28 12:09:40 -0700

    Added timeout for client

--- Client/UDPClient.py
+++ Client/UDPClient.py
@@ -4,29 +4,46 @@ import pickle
 import socket
 import sys
 import time
+import threading
+import select
 
 UDP_IP = "localhost"
-UDP_PORT = 7446
+UDP_PORT = 9999
 # camera = cv2.VideoCapture(0)
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-top_ping = 0
+sock.settimeout(5)
 pings = []
+packets_lost = 0
+start = 0
+end = 0
 
 while True:
-    data = "".encode('utf-8')
-    start = time.time()
+    data = "W".encode('utf-8')
     sock.sendto(data, (UDP_IP, UDP_PORT))
-    bytes_data = sock.recv(65536)
+    start = time.time()
+    try:
+        bytes_data = sock.recv(65536)
+    except socket.timeout as e:
+        print("ERROR! CONNECTION LOST!")
+        print("\nEXITING")
+        sys.exit(0)
+    except ConnectionResetError as e:
+        print("ERROR! COULD NOT ESTABLISH A CONNECTION!")
+        sys.exit(0)
     end = time.time()
-    data = pickle.loads(bytes_data)
-    # print(type(data))
-    # grabbed, frame = camera.read()
-    # frame = cv2.flip(frame, 1)
-    decimg = cv2.imdecode(data, 1)
-    cv2.imshow("Jetson Camera", decimg)
-    ping = ((end-start) * 1000)
-    print("Ping: " + str(ping))
-    pings.append(ping)
+    if data == "":
+        print("ERROR! LOST A PACKET!")
+        packets_lost = packets_lost + 1
+    else:
+        data = pickle.loads(bytes_data)
+        # print(type(data))
+        # grabbed, frame = camera.read()
+        # frame = cv2.flip(frame, 1)
+        decimg = cv2.imdecode(data, 1)
+        cv2.imshow("Jetson Camera", decimg)
+        ping = ((end-start) * 1000)
+        print("Ping: " + str(ping))
+        pings.append(ping)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         top_ping = 0
         all_pings = 0
@@ -34,9 +51,11 @@ while True:
             if ping > top_ping:
                 top_ping = ping
             all_pings += ping
+        print("\nEXITING\n-----------------")
         print("Total Pings: " + str(len(pings)))
         print("Top Ping: " + str(top_ping))
         print("Average Ping: " + str(all_pings/len(pings)))
+        print("Packets Lost: " + str(packets_lost))
         break
 
 cv2.destroyAllWindows()
--- Server/Server.py
+++ Server/Server.py
@@ -6,15 +6,14 @@ class UDPHandler(socketserver.BaseRequestHandler):
     def handle(self):
         initial = self.request[0].strip()
         socket = self.request[1]
+        print("CONNECTED: " + str(initial.decode('utf-8')))
         grabbed, feed = camera.read()
         encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 40]
         result, encimg = cv2.imencode('.jpg', feed, encode_param)
         feed_as_bytes = pickle.dumps(encimg)
-        # print(type(encimg))
-        # print(type(feed_as_bytes))
         socket.sendto(feed_as_bytes, self.client_address)
 
 camera = cv2.VideoCapture(0)
-HOST, PORT = "localhost", 7446
+HOST, PORT = "localhost", 9999
 server = socketserver.UDPServer((HOST, PORT), UDPHandler)
 server.serve_forever()
