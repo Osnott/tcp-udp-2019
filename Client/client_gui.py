180fd1693a23db28ef839b6c03ec7619021e0952
Osnott <aidenonstott@gmail.com>
2019-02-11 16:20:57 -0700

    Fixed UI things, added dark theme

--- Client/client_gui.py
+++ Client/client_gui.py
@@ -1,21 +1,21 @@
-import tkinter
+import tkinter as tk
 import tkinter.ttk as ttk
 
 serverData = {}
 ready = False
 exited = False
-m = tkinter.Tk()
-m.title('Yeet')
-ttk.Label(m, text='IP').grid(row=0)
-ttk.Label(m, text='Port').grid(row=1)
-e1 = ttk.Entry(m)
-e2 = ttk.Entry(m)
+m = tk.Tk()
+m.title('UDP Client')
+ip = ttk.Label(m, background="#282f38", foreground="#eaeaea", text='IP').grid(row=0)
+port = ttk.Label(m, text='Port', background="#282f38", foreground="#eaeaea").grid(row=1)
+e1 = tk.Entry(m, background="#474c56", foreground="#eaeaea")
+e2 = tk.Entry(m, background="#474c56", foreground="#eaeaea")
 e1.grid(row=0, column=1)
 e2.grid(row=1, column=1)
-e1.insert(0, '')
+e1.insert(0, '192.168.1.7')
 e2.insert(0, '9999')
-var1 = tkinter.IntVar()
-ttk.Checkbutton(m, text='Debug', variable=var1).grid(row=2, column=1)
+var1 = tk.IntVar()
+checkbox = tk.Checkbutton(m, background="#282f38", foreground="#a8acb5", text='Debug', variable=var1).grid(row=2, column=1)
 
 
 def startServer():
@@ -35,7 +35,9 @@ def exit_me():
     m.destroy()
 
 
-ttk.Button(m, text='Start Server', width=25, command=startServer).grid(row=3, column=1)
-ttk.Button(m, text='Exit', width=25, command=exit_me).grid(row=3, column=0)
+startButton = tk.Button(m, background="#474c56", foreground="#eaeaea", text='Start Server', width=25, command=startServer).grid(row=3, column=1)
+exitButton = tk.Button(m, background="#474c56", foreground="#eaeaea", text='Exit', width=25, command=exit_me).grid(row=3, column=0)
+
+m.configure(bg='#282f38')
 
 m.mainloop()
--- Server/Server.py
+++ Server/Server.py
@@ -8,6 +8,7 @@ class DriverstationConnectionHandler(socketserver.BaseRequestHandler):
     def handle(self):
         # initial = self.request[0].strip()
         socket = self.request[1]
+        print("CONNECTED")
         packets = 0
         while True:
             # print("CONNECTED: " + str(initial.decode('utf-8')))
@@ -19,7 +20,7 @@ class DriverstationConnectionHandler(socketserver.BaseRequestHandler):
             packets = packets + 1
 
 
-camera = cv2.VideoCapture(0)
+camera = cv2.VideoCapture(1)
 HOST, PORT = str(sys.argv[1]), 9999  # 192.168.43.235
 print("STARTING SERVER ON " + HOST + " ON PORT " + str(PORT))
 server = socketserver.UDPServer((HOST, PORT), DriverstationConnectionHandler)
@@ -31,12 +32,12 @@ server.serve_forever()
 #     def __init__(self):
 #         self._HOST, self._PORT = netifaces.ifadresses("eth0")[netifaces.AF_INET][0]["addr"], 9999
 #         self._server = socketserver.UDPServer((self._HOST, self._PORT), DriverstationConnectionHandler())
-    
+#
 #     def run(self):
 #         try:
 #             self._server.serve_forever()
 #         except Exception:
 #             self.stop()
-    
+#
 #     def stop(self):
 #         self._server.shutdown()
